d9952530298e2e1e09bc3a2d70d6d12309a32c9e
ancag <anca.grosan@gmail.com>
2018-12-24 20:11:46 +0200

    Moved the form logic in a new component, show posted time ago.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import fire from './fire';
-import { CountryDropdown } from 'react-country-region-selector';
+
+import JavascriptTimeAgo from 'javascript-time-ago'
+import TimeAgo from 'react-time-ago/no-tooltip'
+import en from 'javascript-time-ago/locale/en'
+
+import Form from './Form.js'
+
+JavascriptTimeAgo.locale(en)
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = { 
       messages: [], 
-      text: '', 
-      country: '', 
-      type: 'general',
-      textareaFontSize: '2',
-      formValid: false
     };
   }
   componentWillMount(){
@@ -24,114 +26,57 @@ class App extends Component {
         text: snapshot.val().text,
         type: snapshot.val().type,
         country: snapshot.val().country,
+        timestamp: snapshot.val().timestamp,
+        voteCount: snapshot.val().voteCount,
         id: snapshot.key
       };
       
       this.setState({ messages: [message].concat(this.state.messages) });
     })
   }
-  addMessage(e){
-    e.preventDefault();
-    
-    fire.database().ref('messages').push({
-      text: this.state.text,
-      type: this.state.type,
-      country: this.state.country
-    });
 
-    this.setState({
-      text: '',
-      country: '',
-      type: 'general',
-    });    
-  }
-
-  selectType(e){
-    this.setState({ type: e.target.value });
-  }
-  selectCountry (val) {
-    this.setState({ country: val });
-  }
-  textareaChange(e) {
-    let length = e.target.value.length,
-      fontSize = this.state.textareaFontSize,
-      formValid = this.state.formValid;
-
-    if (e.target.value.trim().length > 0){
-      formValid = true
-    } else{
-      formValid = false
-    }
-
-    if(length>120){
-      fontSize = 1;
-    } else if (length>90){
-      fontSize = 1.25;
-    } else if (length>60){
-      fontSize = 1.5;
-    } else if (length>30){
-      fontSize = 1.75;
-    } else {
-      fontSize = 2;
-    }
-
-    this.setState({
-      text: e.target.value,
-      textareaFontSize: fontSize,
-      formValid: formValid
-    });
+  upVote(e) {
+    console.log("clicking up: ", this.message.voteCount);
+    let count = !!this.message.voteCount
+    this.setState({ voteCount: count+1 });
   }
 
   render() {
-    const { country } = this.state;
-
     return (
       <div className="feedContainer">
-        <form onSubmit={this.addMessage.bind(this)}>
-          <textarea 
-            className="new-superstition"
-            style={{fontSize: this.state.textareaFontSize + 'rem'}}
-            type="text" 
-            onChange={this.textareaChange.bind(this)}
-            placeholder="What's your superstition about, is it personal, where is it from?"
-            rows="6"
-            value={this.state.text} />
-          
-          <div>
-            <input 
-              onChange={this.selectType.bind(this)} 
-              type="radio" 
-              value="general" 
-              name="type" 
-              checked={this.state.type==="general"}/>
-            General
-            
-            <input 
-              onChange={this.selectType.bind(this)} 
-              type="radio" 
-              value="personal" 
-              checked={this.state.type==="personal"}
-              name="type"/>
-            Personal
-          </div>
 
-          <CountryDropdown onChange={(val) => this.selectCountry(val)} value={country}/>
-          
-          <button 
-            disabled={! this.state.formValid}
-            type="submit"
-            className="add-btn">
-            ADD
-          </button>
-        </form>
+        <Form/>
 
         <div className="feed">
           <ul>
             { /* Render the list of superstitions */
-              this.state.messages.map( message => <li key={message.id}>
+              this.state.messages.map( message => 
+              <li key={message.id}>
                 {message.text} 
                 <hr/>
-                type: {message.type}, origin: {message.country}
+                type: {message.type}
+
+                {message.country &&
+                  <span>
+                    origin:&nbsp;
+                    {message.country}
+                  </span>
+                }
+                
+                {message.timestamp && 
+                  <span>posted on:&nbsp;
+                    <TimeAgo>
+                      {message.timestamp}
+                    </TimeAgo>
+                  </span>
+                }
+
+                <span 
+                  className="upvotes"
+                  onClick={this.upVote.bind(this)}>
+                  count: -{message.voteCount}-
+                  up
+                </span>
               </li> )
             }
           </ul>
---
+++ src/Form.js
@@ -0,0 +1,117 @@
+import React, { Component } from 'react';
+import fire from './fire';
+import { CountryDropdown } from 'react-country-region-selector';
+
+class Form extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+          text: '', 
+          country: '', 
+          type: 'general',
+          textareaFontSize: '2',
+          formValid: false
+        };
+      }
+
+    addMessage(e){
+        e.preventDefault();
+
+        const timestamp = Date.now();
+        
+        fire.database().ref('messages').push({
+            text: this.state.text,
+            type: this.state.type,
+            country: this.state.country,
+            timestamp: timestamp
+        });
+
+        this.setState({
+            text: '',
+            country: '',
+            type: 'general',
+            timestamp: timestamp
+        });    
+    }
+
+    selectType(e){
+        this.setState({ type: e.target.value });
+    }
+    selectCountry (val) {
+        this.setState({ country: val });
+    }
+    textareaChange(e) {
+        let length = e.target.value.length,
+            fontSize = this.state.textareaFontSize,
+            formValid = this.state.formValid;
+
+        if (e.target.value.trim().length > 0){
+            formValid = true
+        } else{
+            formValid = false
+        }
+
+        if(length>120){
+            fontSize = 1;
+        } else if (length>90){
+            fontSize = 1.25;
+        } else if (length>60){
+            fontSize = 1.5;
+        } else if (length>30){
+            fontSize = 1.75;
+        } else {
+            fontSize = 2;
+        }
+
+        this.setState({
+            text: e.target.value,
+            textareaFontSize: fontSize,
+            formValid: formValid
+        });
+    }
+    render() {
+        const { country } = this.state;
+
+        return (
+            <form onSubmit={this.addMessage.bind(this)}>
+                <textarea 
+                    className="new-superstition"
+                    style={{fontSize: this.state.textareaFontSize + 'rem'}}
+                    type="text" 
+                    onChange={this.textareaChange.bind(this)}
+                    placeholder="What's your superstition about, is it personal, where is it from?"
+                    rows="6"
+                    value={this.state.text} />
+                
+                <div>
+                    <input 
+                        onChange={this.selectType.bind(this)} 
+                        type="radio" 
+                        value="general" 
+                        name="type" 
+                        checked={this.state.type==="general"}/>
+                    General
+                    
+                    <input 
+                        onChange={this.selectType.bind(this)} 
+                        type="radio" 
+                        value="personal" 
+                        checked={this.state.type==="personal"}
+                        name="type"/>
+                    Personal
+                </div>
+
+                <CountryDropdown onChange={(val) => this.selectCountry(val)} value={country}/>
+                
+                <button 
+                    disabled={! this.state.formValid}
+                    type="submit"
+                    className="add-btn">
+                    ADD
+                </button>
+            </form>
+        );
+    }
+}
+
+export default Form;
