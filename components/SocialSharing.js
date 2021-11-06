8c05708d12d7fe2d0db89322bb63104ba1e916fd
ancagrosan <anca.grosan@lola.tech>
2021-11-06 22:05:12 +0200

    fix: facebook shared text fix

--- components/SocialSharing.js
+++ components/SocialSharing.js
@@ -15,31 +15,39 @@ const options = [
   {
     button: FacebookShareButton,
     icon: FacebookIcon,
+    textProp: "quote",
   },
   {
     button: TelegramShareButton,
     icon: TelegramIcon,
+    textProp: "title",
   },
   {
     button: TwitterShareButton,
     icon: TwitterIcon,
+    textProp: "title",
   },
   {
     button: WhatsappShareButton,
     icon: WhatsappIcon,
+    textProp: "title",
   },
 ];
 
 const SocialSharing = ({ shareURL, title }) => {
   return (
     <ul className="social-sharing-list">
-      {options.map((op, index) => (
-        <li key={index}>
-          <op.button url={shareURL} title={title}>
+      {options.map((op, index) => {
+        const buttonProps = {
+          url: shareURL,
+          [op.textProp]: title
+        };
+        return <li key={index}>
+          <op.button {...buttonProps}>
             <op.icon size={30} round />
           </op.button>
-        </li>
-      ))}
+        </li>;
+      })}
     </ul>
   );
 };
